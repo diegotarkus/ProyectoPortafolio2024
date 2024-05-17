9c96688d84d5327ad5cec7aa1fa5245a93e73107
Diego Galarce <dgalarcef@gmail.com>
2024-05-17 12:31:50 -0400

    CRUD completo
    Nuevos cambios:
    - CRUD completo (modificar y eliminar datos desde la página)
    * commit anterior: importar librería "bcrypt" (para la encriptación y validación de las contraseñas).

--- gestionPedidos/urls.py
+++ gestionPedidos/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('producto_nuevo/', producto_nuevo, name='producto_nuevo'),
     path('productos/', lista_productos, name='productos'),
+    path('producto_nuevo/', producto_nuevo, name='producto_nuevo'),
+    path('productos/editar/<str:id_producto>', producto_editar, name='producto_editar'),
+    path('productos/<str:id_producto>/borrar', producto_borrar, name='producto_borrar')
 ]
 
--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -29,6 +29,32 @@ def producto_nuevo(request):
         form = ProductoList    
     return render(request, "producto_nuevo.html", {'form' : form})
 
+def producto_editar(request, id_producto):
+    try:
+        producto = Producto.objects.get(id_producto = id_producto)
+        form = ProductoList(instance = producto)
+
+        if request.method == 'POST':
+            form = ProductoList(request.POST, request.FILES, instance = producto)
+            if form.is_valid():
+                form.save()
+                return redirect(reverse('productos') + '?UPDATED')
+            else:
+                return redirect(reverse('producto_editar') + id_producto) 
+
+        context = {'form' : form}
+        return render(request,'producto_editar.html', context)
+    except:
+        return redirect(reverse('productos') + '?NO_EXISTS')
+    
+def producto_borrar(request, id_producto):
+    try:
+        producto = Producto.objects.get(id_producto = id_producto)
+        producto.delete()
+        return redirect(reverse('productos') + '?DELETED')
+    except:
+        return redirect(reverse('productos') + '?FAIL')
+
 def lista_productos(request):
     context = {'productos': Producto.objects.all()}
     return render(request,'productos.html',context)
