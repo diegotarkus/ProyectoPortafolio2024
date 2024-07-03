43ba8e4f89d0ac4646caa6a830e5b4c23c873122
Cristian Gamboa <cristian.gamboa2047@gmail.com>
2024-07-03 16:08:57 -0400

    Agregado pip livereload en INSTALLED_APPS y MIDDLEWARE

--- Restaurant/settings.py
+++ Restaurant/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'livereload',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'psycopg2',
@@ -68,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'Restaurant.urls'
